b5a7ae46a5c86677161fe6cbe491241f489882fb
dhylan01 <dhylan01@users.noreply.github.com>
2021-02-01 22:31:41 -0800

    Unit 01

--- Unit01/src/StarsAndStripes.java
+++ Unit01/src/StarsAndStripes.java
@@ -6,33 +6,53 @@
 //Class -
 //Lab  -
 
-import static java.lang.System.*;
 
+
+	import static java.lang.System.*;
 public class StarsAndStripes
 {
-   public StarsAndStripes()
+
+	public StarsAndStripes()
    {
       out.println("StarsAndStripes");
       printTwoBlankLines();
    }
 
    public void printTwentyStars()
-   {
+   {	
+	   for ( int d = 0; d < 20; d++) {
+	   		System.out.print("*");
    }
+   }
+   
+   
 
    public void printTwentyDashes()
-   {
+   {	for (int i = 0; i < 20; i++) {
+	   System.out.print("-");
+   }
    }
 
    public void printTwoBlankLines()
-   {
+   {	System.out.println("\n\n");
+	   
    }
    
    public void printASmallBox()
-   {	
+   {	System.out.print("************");
+	   for (int i = 0; i < 10; i++) {
+		   System.out.println("*        *");
+	   }
+   System.out.print("************");
    }
  
    public void printABigBox()
-   { 	
+   { 	   {	System.out.print("**********************");
+	   for (int i = 0; i < 20; i++) {
+		   System.out.println("*                 *");
+	   }
+   System.out.print("**********************");
+   }
+ 
    }   
 }
---
+++ Unit01/src/StarsRunner.java
@@ -0,0 +1,35 @@
+//© A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class StarsRunner
+{
+	static StarsAndStripes reference = new StarsAndStripes(); 
+   public static void main(String args[])
+   {
+    for( int i = 0; i < 3; i++) {
+	   reference.printTwentyStars();
+	   System.out.print("\n");
+	   reference.printTwentyDashes();
+	   System.out.print("\n");
+    }
+    reference.printTwoBlankLines();
+    for( int i = 0; i < 3; i++) {
+ 	   reference.printTwentyStars();
+ 	   System.out.print("\n");
+ 	   reference.printTwentyDashes();
+ 	   System.out.print("\n");
+     }
+    reference.printTwentyDashes();
+	   //instantiate a StarsAndStripes object
+      
+      //call the methods needed to make the patterns on the word document
+      
+   }
+}
